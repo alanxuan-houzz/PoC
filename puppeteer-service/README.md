cbf253b6288b6fda0823fb0654333df63332a9d6
alanxuan <alan.xuan@houzz.com>
2020-06-24 21:54:31 +0900

    [Puppeteer Service] Create a simple express app to take screenshot

---
+++ puppeteer-service/README.md
@@ -0,0 +1,16 @@
+## Puppeteer Service
+
+### RUN
+Following command builds and starts a container
+
+```bash
+PORT=9999 npm run start
+```
+
+Open browser and take a screen shot given siteId
+
+```
+http://localhost:9999/screenshot?siteId=${id}
+```
+
+
--- puppeteer-service/service.ts
+++ puppeteer-service/service.ts
@@ -2,20 +2,30 @@ import express from 'express';
 import puppeteer from 'puppeteer';
 
 const app = express();
-const PORT = 9999;
+const PORT = 8080;
 const IMAGE_TYPE = 'png';
 
 const getTemplatePreviewUrl = (siteId: number) => `https://www.houzz.com/website-services/sites/${siteId}/view/`;
 
 (async () => {
     const browser = await puppeteer.launch({
+        args: [
+            // https://developers.google.com/web/tools/puppeteer/troubleshooting#running_puppeteer_in_docker
+            // Required for Docker version of Puppeteer
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            // This will write shared memory files into /tmp instead of /dev/shm,
+            // because Docker’s default for /dev/shm is 64MB
+            '--disable-dev-shm-usage',
+        ],
         headless: true,
         defaultViewport: {
-            width: 1440,
-            height: 960,
+            width: 2440,
+            height: 1024,
             isLandscape: true,
         },
     });
+    process.on('exit', () => browser.close());
     async function takeScreenShot(siteId: number) {
         const page = await browser.newPage();
         try {
@@ -37,5 +47,5 @@ const getTemplatePreviewUrl = (siteId: number) => `https://www.houzz.com/website
         }
         return res.type(IMAGE_TYPE).send(await takeScreenShot(siteId));
     });
-    app.listen(PORT, () => console.log(`started server listening on ${PORT}`));
+    app.listen(PORT, () => console.log(`started puppeteer service`));
 })();
